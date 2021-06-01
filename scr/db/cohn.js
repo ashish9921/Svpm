170bdb52cdf326baec3cbb0e3bff42c0695d5a7b
Ashish <62475101+ashish9921@users.noreply.github.com>
2021-06-01 12:02:02 +0530

    Update cohn.js

--- scr/db/cohn.js
+++ scr/db/cohn.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/Svpm",{ useNewUrlParser: true,useUnifiedTopology: true },{
+mongoose.connect(process.env.MONGODB_URI ||"mongodb://localhost:27017/Svpm",{ useNewUrlParser: true,useUnifiedTopology: true },{
 
 }).then(()=>{
     console.log(` mongo db conection succseful`);
